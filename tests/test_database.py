4946e1f267df989e91e3aaffce20924e15fc657c
Nikita Savin <klaster26@mail.ru>
2024-06-13 15:22:31 +0300

    feat: добавить возможность апдейта города пользователя

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,15 +1,39 @@
 import sys
 
+import pytest
 sys.path.append('../weatherapp')
 sys.path.append('../')
 
 import weatherapp.orm_runner as orm
 
+test_counter = 0
 
-def test_add_tg_user():
-    tg_user_id = 123456789
-    orm.add_user(tg_user_id)
+
+@pytest.fixture()
+def get_user_id():
+    return 123456789
+
+
+@pytest.fixture()
+def get_user_city():
+    return 'Moscow'
+
+
+def test_add_tg_user(get_user_id):
+    orm.add_user(get_user_id)
     session = orm.Session()
-    test_user_query = session.query(orm.User).filter_by(tg_id=tg_user_id).first()
-    assert test_user_query.tg_id == tg_user_id
-    print('Test passed')
+    test_user_query = session.query(orm.User).filter_by(tg_id=get_user_id).first()
+    assert test_user_query.tg_id == get_user_id
+    global test_counter
+    test_counter += 1
+    print(f'Test # {test_counter} passed. New user is added.')
+
+
+def test_add_user_city(get_user_id, get_user_city):
+    orm.add_city(get_user_id, get_user_city)
+    session = orm.Session()
+    test_user_query = session.query(orm.User).filter(orm.User.tg_id == get_user_id).first()
+    assert test_user_query.city == get_user_city
+    global test_counter
+    test_counter += 1
+    print(f'Test # {test_counter} passed. User\'s city is updated.')
--- weatherapp/orm_runner.py
+++ weatherapp/orm_runner.py
@@ -7,7 +7,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-
 load_dotenv()
 
 db_username = os.getenv('PSQL_USER')
@@ -20,7 +19,7 @@ db_name = os.getenv('PSQL_DB_NAME')
 db_url = get_db_connection(
     db_username, db_pswd, db_host, db_port, db_name)
 
-engine = create_engine(db_url, echo=True)
+engine = create_engine(db_url, echo=False)
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
@@ -28,11 +27,16 @@ Session = sessionmaker(bind=engine)
 
 def add_user(tg_id):
     session = Session()
-    user = session.query(User).filter_by(User.tg_id == tg_id).first()
+    user = session.query(User).filter(User.tg_id == tg_id).first()
     if user is None:
         new_user = User(tg_id=tg_id)
         session.add(new_user)
         session.commit()
 
 
-
+def add_city(tg_id, city_name):
+    session = Session()
+    add_user(tg_id)
+    user = session.query(User).filter(User.tg_id == tg_id).first()
+    user.city = city_name
+    session.commit()
